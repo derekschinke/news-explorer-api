521e5955021d51c41afe687f4923a7742c02c785
Derek Schinke <derekschinke@me.com>
2021-04-29 15:28:07 -0400

    feature: user schema complete

--- models/user.js
+++ models/user.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-const schema = new mongoose.Schema({ email: { type: String, required: true } });
+const schema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    validate: { validator: (email) => validator.isEmail(email) },
+  },
+  password: { type: String, required: true },
+  name: { type: String, minLength: 2, maxLength: 30, required: true },
+});
 
 module.exports = mongoose.model('user', schema);
