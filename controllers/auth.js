18313fcbd8294add765b21a5ea001db7ec587d4c
Derek Schinke <derekschinke@me.com>
2021-04-30 00:29:37 -0400

    feature: error handling signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
 const dotenv = require('dotenv');
 const bcrypt = require('bcrypt');
+
 const User = require('../models/user');
 const { STATUS_CODES } = require('../utils/constants');
+const BadRequestError = require('../errors/BadRequestError');
+const ConflictError = require('../errors/ConflictError');
 
 dotenv.config();
 // const { NODE_ENV, JWT_SECRET } = process.env;
@@ -15,6 +18,14 @@ module.exports.signUp = (req, res, next) => {
       const cleanedUser = user.toObject();
       delete cleanedUser.password;
       res.status(STATUS_CODES.created).send(cleanedUser);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new BadRequestError('Unable to create user');
+      } else if (err.name === 'MongoError') {
+        throw new ConflictError('User already exists');
+      }
+      next(err);
     })
     .catch(next);
 };
