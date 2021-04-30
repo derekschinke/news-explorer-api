7b7e5b2e66178fd0848644e28578a990958fd05a
Derek Schinke <derekschinke@me.com>
2021-04-30 16:44:53 -0400

    style: renamed auth to authRouter in import

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const auth = require('./auth');
+const authRouter = require('./auth');
 // const users = require('./users');
 
 // const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.use('/', auth);
+router.use('/', authRouter);
 // router.use('/users', auth, users);
 
 module.exports = router;
