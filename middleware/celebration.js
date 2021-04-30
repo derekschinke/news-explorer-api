854960b1e42ec7c8c3d7f79b7eedb7d384c700d0
Derek Schinke <derekschinke@me.com>
2021-04-30 00:21:35 -0400

    feature: signupcelebration

---
+++ middleware/celebration.js
@@ -0,0 +1,9 @@
+const { celebrate, Joi } = require('celebrate');
+
+module.exports.signUpCelebration = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30).required(),
+  }),
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const { signUp } = require('../controllers/auth');
+const { signUpCelebration } = require('../middleware/celebration');
 
 const router = express.Router();
 
-router.post('/signup', signUp);
+router.post('/signup', signUpCelebration, signUp);
