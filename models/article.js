d44de8b5f367b20c32b5c54d6315ee1bc42f8741
Derek Schinke <derekschinke@me.com>
2021-04-30 16:09:45 -0400

    fix: remove versionKey from mongoose schemas

--- models/article.js
+++ models/article.js
@@ -1,42 +1,45 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const schema = new mongoose.Schema({
-  keyword: {
-    type: String,
-    required: true,
+const schema = new mongoose.Schema(
+  {
+    keyword: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    source: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+      validate: { validator: (url) => validator.isUrl(url) },
+    },
+    image: {
+      type: String,
+      required: true,
+      validate: { validator: (url) => validator.isUrl(url) },
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      select: false,
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  text: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: String,
-    required: true,
-  },
-  source: {
-    type: String,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: true,
-    validate: { validator: (url) => validator.isUrl(url) },
-  },
-  image: {
-    type: String,
-    required: true,
-    validate: { validator: (url) => validator.isUrl(url) },
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    select: false,
-  },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model('article', schema);
--- models/user.js
+++ models/user.js
@@ -1,24 +1,27 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const schema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: { validator: (email) => validator.isEmail(email) },
+const schema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: { validator: (email) => validator.isEmail(email) },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    name: {
+      type: String,
+      minLength: 2,
+      maxLength: 30,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-  name: {
-    type: String,
-    minLength: 2,
-    maxLength: 30,
-    required: true,
-  },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model('user', schema);
