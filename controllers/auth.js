e4351e0d4aee2de965f1129f56a78fd77ba27d7e
Derek Schinke <derekschinke@me.com>
2021-04-30 00:02:39 -0400

    fix: signUp returns user and status_code constant

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const { STATUS_CODES } = require('../utils/constants');
 
 dotenv.config();
 // const { NODE_ENV, JWT_SECRET } = process.env;
@@ -10,5 +11,10 @@ module.exports.signUp = (req, res, next) => {
   bcrypt
     .hash(password, 10)
     .then((hash) => User.create({ email, password: hash, name }))
+    .then((user) => {
+      const cleanedUser = user.toObject();
+      delete cleanedUser.password;
+      res.status(STATUS_CODES.created).send(cleanedUser);
+    })
     .catch(next);
 };
---
+++ utils/constants.js
@@ -0,0 +1,3 @@
+module.exports.BEARER_REGEX = /^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/;
+
+module.exports.STATUS_CODES = { ok: 200, created: 201 };
