b74b53469e37cb0a81caee19c66b7fe60de4ab7b
Derek Schinke <derekschinke@me.com>
2021-04-29 21:36:55 -0400

    fix: app port from 3001 to 3000

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require('./middleware/logger');
 
 dotenv.config();
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
